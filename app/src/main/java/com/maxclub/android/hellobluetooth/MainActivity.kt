ef0bf26e0cbc585c19fd0934c98d287c03e2df64
MaxKliuba <klyubamaks@gmail.com>
2022-04-14 00:30:13 +0300

    add navigation

--- app/src/main/java/com/maxclub/android/hellobluetooth/MainActivity.kt
+++ app/src/main/java/com/maxclub/android/hellobluetooth/MainActivity.kt
@@ -2,10 +2,63 @@ package com.maxclub.android.hellobluetooth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var onDestinationChangedListener: NavController.OnDestinationChangedListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById<NavigationView>(R.id.navigationView).apply {
+            setupWithNavController(navController)
+        }
+        val topLevelDestinationIds = setOf(
+            R.id.connectionFragment,
+            R.id.terminalFragment,
+        )
+        appBarConfiguration = AppBarConfiguration(topLevelDestinationIds, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        onDestinationChangedListener =
+            NavController.OnDestinationChangedListener { controller, destination, arguments ->
+                supportActionBar?.subtitle = if (topLevelDestinationIds.contains(destination.id)) {
+                    getString(R.string.state_disconnected)
+                } else {
+                    ""
+                }
+            }
     }
+
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(onDestinationChangedListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navController.removeOnDestinationChangedListener(onDestinationChangedListener)
+    }
+
+    override fun onSupportNavigateUp(): Boolean =
+        navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 }
